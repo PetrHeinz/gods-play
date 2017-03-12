0d6639a22da51672760b53e5302709b0f76ebd34
Petr Heinz <noobos@gmail.com>
2017-03-12 21:30:21 +0100

    Removed unnecessary HTML tag

--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@
 
     var board = boardGenerator.generateBoard(boardSize);
 
-    var renderElement = document.getElementById('game-board');
-
     var game = new Game(board);
     var renderer = new Renderer(board, game);
 
-    renderElement.appendChild(renderer.getView());
+    document.body.appendChild(renderer.getView());
 
     renderer.createBoard();
 
