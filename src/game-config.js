3583b70e95b2c5319020d1d2ffb404bf12cc408f
Petr Heinz <noobos@gmail.com>
2017-04-30 16:49:22 +0200

    Refactoring: GameConfig.createCellConfig is now in function scope of constructor
    - has no access to own properties or method calls, usable only in constructor itself

--- src/game-config.js
+++ src/game-config.js
@@ -18,28 +18,28 @@ export default class GameConfig {
 
     /** @type {CellConfig[]} */
     this.cellConfigs = [
-      this.createCellConfig('brick', '♖', new CellRange(0)),
-      this.createCellConfig('grass', '♘', new CellRange(2)),
-      this.createCellConfig('tree', '♗', new CellRange(1)),
-      this.createCellConfig('stone', '♜', new CellRange(0)),
-      this.createCellConfig('sand', '♞', new CellRange(2)),
-      this.createCellConfig('wheat', '♝', new CellRange(1)),
-      this.createCellConfig('water')
+      createCellConfig('brick', '♖', new CellRange(0)),
+      createCellConfig('grass', '♘', new CellRange(2)),
+      createCellConfig('tree', '♗', new CellRange(1)),
+      createCellConfig('stone', '♜', new CellRange(0)),
+      createCellConfig('sand', '♞', new CellRange(2)),
+      createCellConfig('wheat', '♝', new CellRange(1)),
+      createCellConfig('water')
     ]
-  }
 
-  /**
-   * @param {string} terrain
-   * @param {string} [unitSymbol]
-   * @param {CellRange} [range]
-   * @return {CellConfig}
-   */
-  createCellConfig (terrain, unitSymbol, range) {
-    let unitConfig = null
-    if (unitSymbol !== undefined) {
-      unitConfig = new UnitConfig(unitSymbol, range)
+    /**
+     * @param {string} terrain
+     * @param {string} [unitSymbol]
+     * @param {CellRange} [range]
+     * @return {CellConfig}
+     */
+    function createCellConfig (terrain, unitSymbol, range) {
+      let unitConfig = null
+      if (unitSymbol !== undefined) {
+        unitConfig = new UnitConfig(unitSymbol, range)
+      }
+
+      return new CellConfig(terrain, unitConfig)
     }
-
-    return new CellConfig(terrain, unitConfig)
   }
 }
