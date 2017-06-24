3e958d53f12c8ab5398fbba5a6cd5612b38ec8c1
Petr Heinz <noobos@gmail.com>
2017-06-24 18:07:15 +0200

    coordinate distance of cells on different boards is infinity

--- src/Game/Cell/function/distance.js
+++ src/Game/Cell/function/distance.js
@@ -4,9 +4,22 @@
  * @return {number}
  */
 export function coordinateDistance (origin, target) {
+  if (!isOnSameBoard(origin, target)) {
+    return Infinity
+  }
+
   return Math.max(
     Math.abs(origin.coordinate.x - target.coordinate.x),
     Math.abs(origin.coordinate.y - target.coordinate.y),
     Math.abs(origin.coordinate.z - target.coordinate.z)
   )
+}
+
+/**
+ * @param {Cell} origin
+ * @param {Cell} target
+ * @return {boolean}
+ */
+export function isOnSameBoard (origin, target) {
+  return origin.parent.children.indexOf(target) > -1
 }
